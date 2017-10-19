25359f5a1be00a322f900d4bbf949685aa82be21
seaboss <bdlee@mtcom.co.kr>
2017-10-19 16:29:21 +0900

    namespace, struct

--- streamExample/streamExample/main.cpp
+++ streamExample/streamExample/main.cpp
@@ -1,8 +1,34 @@
 #include <stdio.h>
 #include <iostream>
 
+//namespace Vector2D
+//{
+//	int x;
+//	int y;
+//	void print()
+//	{
+//		printf("%d %d", x, y);
+//	}
+//}
+struct Vector2D
+{
+	int x;
+	int y;
+	void print()
+	{
+		printf("%d %d", x, y);
+	}
+}
+
 void main()
 {
-	printf("This is C style!");
-	std::cout <<" This is C++ style" << std::endl;
+	Vector2D my_vector;
+	//Vector2D:: x = 1;
+	//Vector2D:: y = 2;
+	//Vector2D::print();
+	my_vector.x = 1;
+	my_vector.y = 2;
+	my_vector.print();
+	//printf("This is C style!");
+	//std::cout <<" This is C++ style" << std::endl;
 }
