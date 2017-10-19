e9e1b926f89c2312572d5e08b45fc092e7068361
seaboss <bdlee@mtcom.co.kr>
2017-10-19 16:59:45 +0900

    file 출력

--- streamExample/streamExample/main.cpp
+++ streamExample/streamExample/main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
 
 //namespace Vector2D
 //{
@@ -10,6 +11,7 @@
 //		printf("%d %d", x, y);
 //	}
 //}
+
 struct Vector2D
 {
 	int x;
@@ -18,17 +20,53 @@ struct Vector2D
 	{
 		printf("%d %d", x, y);
 	}
+};
+
+//class Vector2D
+//{
+//public:
+//	int x;
+//	int y;
+//	void print()
+//	{
+//		printf("%d %d", x, y);
+//	}
+//};
+
+std::ostream &operator << (std::ostream &stream, Vector2D obj) 
+{
+	stream << obj.x << "" << obj.y << std::endl;
+	return stream;
 }
 
 void main()
 {
+	//struct Vector2D
 	Vector2D my_vector;
-	//Vector2D:: x = 1;
-	//Vector2D:: y = 2;
-	//Vector2D::print();
 	my_vector.x = 1;
 	my_vector.y = 2;
 	my_vector.print();
+	
+	//c style
+	//FILE *of = fopen("cstyle.txt","w");
+	//fprintf(of, "%d %d", my_vector.x, my_vector.y);
+	//fclose(of);
+	
+	//namespace Vector2D
+	//Vector2D:: x = 1;
+	//Vector2D:: y = 2;
+	//Vector2D::print();
+
 	//printf("This is C style!");
 	//std::cout <<" This is C++ style" << std::endl;
+
+	/*
+	using namespace std;
+	std::cout << my_vector.x <<" This is C++ style"<< my_vector.y << std::endl;
+	*/
+	std::cout << my_vector << std::endl;
+
+	std::ofstream ofile("cppstyle.txt");
+	ofile << my_vector << std::endl;
+
 }
